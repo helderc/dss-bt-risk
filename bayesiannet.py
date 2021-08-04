516e3cd016f97a3a664fd61e396e800709502e25
Helder <heldercro@gmail.com>
2021-08-03 20:24:59 -0600

    add: class for bayesian nets

---
+++ bayesiannet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+ 
+'''
+File      :   bayesiannet.py
+Created on:   2021/08/03 20:22:34
+ 
+@Author   :   Helder C. R. Oliveira
+@Version  :   1.0
+@Contact  :   helder.ro@outlook.com
+@License  :   (C) Copyright 2021, Helder C. R. Oliveira
+@Desc     :   None
+'''
+ 
+
