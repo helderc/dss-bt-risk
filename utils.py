489339c3f6e4d2418e5dc18d60406d0317e384c7
Helder <heldercro@gmail.com>
2021-08-31 19:00:47 -0600

    add: general purposes file

---
+++ utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+ 
+'''
+File      :   utils.py
+Created on:   2021/08/31 18:59:58
+ 
+@Author   :   Helder C. R. Oliveira
+@Version  :   1.0
+@Contact  :   helder.ro@outlook.com
+@License  :   (C) Copyright 2021, Helder C. R. Oliveira
+@Desc     :   None
+'''
+ 
