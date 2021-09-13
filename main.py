f40b052625792599751e2423afac5b12a3387d85
Helder <heldercro@gmail.com>
2021-09-12 23:38:52 -0600

    fix: cleaning

--- main.py
+++ main.py
@@ -401,48 +401,7 @@ class MainWindow(QMainWindow):
     def Exit(self):
         # QtWidgets.qApp.quit
         self.close()
-
-    # TODO: Maybe this will be necessary when plotting new graphs
-    def updateGraph(self):
-        """Updates graph respective to status of toggled checkbox.
-
-        Retrieves sender (toggled checkbox) reference and if it is checked, retrieves data from model 
-        and passes it to drawLine(). If not checked, references line respective to checkbox and removes
-        it.
-        """
-        checkbox = self.sender()
-        checkboxNumber = int(checkbox.objectName())
-        if checkbox.isChecked():
-            peak = self._model.get_peak(checkboxNumber)
-            duration = self._model.get_duration(checkboxNumber)
-            self.drawLine(peak=peak, checkboxNumber=checkboxNumber, duration=duration)
-        else:
-            for line in self.graphWidget.canvas.ax.lines:
-                if line.get_label() == f"Scenario {checkboxNumber+1}":
-                    self.graphWidget.canvas.ax.lines.remove(line)
-            # TODO: Resize after removal of line
-        self.graphWidget.canvas.ax.legend()
-        # NOTE: Legend is not ordered, not too sure if worth addressing or not.
-        self.graphWidget.canvas.draw()
-
-    def drawLine(self, peak, checkboxNumber, duration=100.0):
-        """Generates gaussian wave based off given peak and duration data in order to model disease spread. 
-
-        Processes gaussian wave off peak and duration data, passes x and y arrays to MplWidget.plot(). 
-        Assigns labels to lines for referenced removal.
-
-        Parameters
-        ----------
-        peak : float
-            Desired peak (%) of wave
-        duration : float, optional
-            Desired duration of wave (days), by default 100.0
-        """
-        X = np.linspace(0, duration, 100)
-        y = norm.pdf(X, duration / 2, duration / 6)
-        y = peak / max(y) * y  # Stretches curve to reach peak point
-        self.graphWidget.plot(X, y, label=f"Scenario {checkboxNumber+1}")
-
+        
 
 def main():
     app = QApplication(sys.argv)
